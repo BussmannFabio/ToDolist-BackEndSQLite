3de3a3f0baedcbc9f12e7fb9e588c346af05a3d6
Fabio <fabio12513@gmail.com>
2024-11-14 10:38:23 -0300

    db'funcionando'

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import sqlite3 from 'sqlite3'
-const fs = require('fs')
+import fs from 'fs'
 
 // Ler o arquivo JSON
 const dados = JSON.parse(fs.readFileSync('users3k.json', 'utf8'))
 
-const db = new sqlite3.Database('./banco.db', sqlite3.OPEN_CREATE)
+const db = new sqlite3.Database('./banco.db')
 
 db.run(`
   CREATE TABLE IF NOT EXISTS users (
