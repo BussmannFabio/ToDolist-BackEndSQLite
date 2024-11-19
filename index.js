561040ba0ffdf46c64d3fc792bc5bbcdab57dabf
Fabio <fabio12513@gmail.com>
2024-11-19 09:16:24 -0300

    2.0

--- index.js
+++ index.js
@@ -4,86 +4,75 @@ import sqlite3 from 'sqlite3'
 const app = express()
 const port = 3005
 
-
 const db = new sqlite3.Database('./banco.db')
 
+const helloDev = (req, res) => res.send('Hello Dev')
 
-app.get('/', (req, res) => {
-  res.send('Hello Dev')
-})
-
-app.get('/users', (req, res) => {
+const users = (req, res) => {
   db.all('SELECT * FROM users', [], (err, rows) => {
-    if (err) {
-      return res.status(500).send("Erro ao executar a consulta!")
-    }
+    if (err) return res.status(500).send("Erro ao executar a consulta!")
     res.json(rows)
   })
-})
+}
 
-app.get('/users/search', (req, res) => {
+const checkingQuery = (req, res, next) => {
   const {
     id, name, email, username, age, country,
     id_maiorq, id_menorq, age_maiorq, age_menorq, page = 1, limite = 5, sortBy = 'id', order = 'asc'
-  } = req.query;
+  } = req.query
 
-  let query = "SELECT * FROM users WHERE"
-  let primeiraCondição = true
+    let query = "SELECT * FROM users WHERE"
+    let primeiraCondição = true
 
-  if ((id && (id_maiorq))) {
-    return res.status(500).send("Impossível buscar por: id e id_maiorq ao mesmo tempo!")
-  }
+      if (id && id_maiorq) return res.status(501).send("Impossível buscar por: id e id_maiorq ao mesmo tempo!")
+      if (id && id_menorq) return res.status(501).send("Impossível buscar por: id e id_menorq ao mesmo tempo!")
+      
+      if (age && age_maiorq) return res.status(501).send("Impossível buscar por: age e age_maiorq ao mesmo tempo!")
+      if (age && age_menorq) return res.status(501).send("Impossível buscar por: age e age_menorq ao mesmo tempo!")
 
-  if ((id && (id_menorq))) {
-    return res.status(500).send("Impossível buscar por: id e id_menorq ao mesmo tempo!")
-  }
+          const addCondition = (condition) => {
+            if (primeiraCondição) {
+              query += ` ${condition}`
+              primeiraCondição = false
+            } else {
+              query += ` AND ${condition}`
+            }
+          }
 
-  if ((age && (age_maiorq))) {
-    return res.status(500).send("Impossível buscar por: age e age_maiorq ao mesmo tempo!")
-  }
+      if (id) addCondition(`id = ${id}`)
+      if (id_maiorq) addCondition(`id > ${id_maiorq}`)
+      if (id_menorq) addCondition(`id < ${id_menorq}`)
 
-  if ((age && (age_menorq))) {
-    return res.status(500).send("Impossível buscar por: age e age_menorq ao mesmo tempo!")
-  }
+      if (name) addCondition(`name LIKE '%${name}%'`)
+      if (email) addCondition(`email LIKE '%${email}%'`)
+      if (username) addCondition(`username LIKE '%${username}%'`)
+      if (country) addCondition(`country LIKE '%${country}%'`)
 
-  const addCondition = (condition) => {
-    if (primeiraCondição) {
-      query += ` ${condition}`
-      primeiraCondição = false
-    } else {
-      query += ` AND ${condition}`
-    }
-  }
+      if (age) addCondition(`age = ${parseInt(age)}`)
+      if (age_maiorq) addCondition(`age > ${parseInt(age_maiorq)}`)
+      if (age_menorq) addCondition(`age < ${parseInt(age_menorq)}`)
+  
+    query += ` ORDER BY ${sortBy} ${order.toUpperCase()}`
+    const offset = (page - 1) * limite
+    query += ` LIMIT ${limite} OFFSET ${offset}`
 
-  if (id) addCondition(`id = ${id}`);
-  if (id_maiorq) addCondition(`id > ${id_maiorq}`)
-  if (id_menorq) addCondition(`id < ${id_menorq}`)
+  req.queryText = query
+  next()
+}
 
-  if (name) addCondition(`name = '${name}'`)
-  if (email) addCondition(`email = '${email}'`)
-  if (username) addCondition(`username = '${username}'`)
+const executeQuery = (req, res) => {
 
-  if (age) addCondition(`age = ${parseInt(age)}`)
-  if (age_maiorq) addCondition(`age > ${parseInt(age_maiorq)}`)
-  if (age_menorq) addCondition(`age < ${parseInt(age_menorq)}`)
+      const query = req.queryText
+      console.log(query)
 
-  if (country) addCondition(`country = '${country}'`)
+      db.all(query, (err, rows) => {
+        if (err) return res.status(500).send("Erro ao executar a consulta!")
+        res.json(rows)
+      })
+}
 
-  query += ` ORDER BY ${sortBy} ${order.toUpperCase()}`
+app.get('/', helloDev)
+app.get('/users', users)
+app.get('/users/search', checkingQuery, executeQuery)
 
-  const offset = (page - 1) * limite
-  query += ` LIMIT ${limite} OFFSET ${offset}`
-
-  console.log(query)
-
-  db.all(query, (err, rows) => {
-    if (err) {
-      return res.status(500).send("Erro ao executar a consulta!")
-    }
-    res.json(rows)
-  })
-})
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta: ${port}`)
-})
+app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta: ${port}`))
