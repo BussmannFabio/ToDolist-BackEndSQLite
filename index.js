cd3e1d7e3e166fddd8ed8a5efc7ecb48a44d3d5e
Fabio <fabio12513@gmail.com>
2024-11-21 07:28:12 -0300

    2.2

--- index.js
+++ index.js
@@ -6,58 +6,6 @@ const port = 3005
 
 const db = new sqlite3.Database('./banco.db')
 
-const validateUser = (req, res, next) => {
-  const { name, email, age, registered_date, country, username } = req.body
-
-    if (!name || typeof name !== 'string' || !name.trim().includes(' '))
-      return res.status(500).json({ error: "Nome inválido. É necessário pelo menos dois nomes com um espaço entre eles" })
-    
-    if (!age || isNaN(age) || age < 8 || age > 120)
-      return res.status(500).json({ error: "Idade inválida. A idade deve ser um número entre 8 e 120 anos" })
-   
-    if (!registered_date || typeof registered_date !== 'string' || registered_date.length !== 10)
-      return res.status(500).json({ error: "Data inválida. A data deve ser uma string com 8 caracteres" })
-   
-    if (!username || typeof username !== 'string' || username.length < 6)
-      return res.status(500).json({ error: "Username inválido. Deve ser uma string com pelo menos 6 caracteres" })
-   
-    if (email && typeof email !== 'string')
-      return res.status(500).json({ error: "Email inválido" })
-   
-    if (country && typeof country !== 'string')
-      return res.status(500).json({ error: "País inválido" })
-
-  next()
-}
-
-const createUser = (req, res) => {
-  const { name, age, email, country, username } = req.body
-
-  const insertQuery = `
-    INSERT INTO users (name, email, username, age, country, registered_date)
-    VALUES (?, ?, ?, ?, ?, GETDATE())
-  `
-
-  const params = [name, email, username, age, country]
-
-  db.run(insertQuery, params, function (err) {
-    if (err)
-      return res.status(500).json({ error: "Erro ao criar o usuário" })
-
-    const maxIdQuery = `SELECT MAX(id) AS id_max FROM users`
-
-    db.get(maxIdQuery, (err, row) => {
-      if (err)
-        return res.status(500).json({ error: "ERRo" })
-
-      res.status(201).json({
-        message: 'Usuário criado com sucesso',
-        userId: row.id_max,
-      })
-    })
-  })
-}
-
 const helloDev = (req, res) => res.send('Hello Dev')
 
 const users = (req, res) => {
@@ -112,10 +60,59 @@ const executeQuery = (req, res) => {
   })
 }
 
+app.use(express.json())
+const validateUser = (req, res, next) => {
+  const { name, email, age, country, username } = req.body
+
+    if (!name || typeof name !== 'string' || !name.trim().includes(' '))
+      return res.status(500).json({ error: "Nome inválido. É necessário pelo menos dois nomes com um espaço entre eles" })
+    
+    if (!age || isNaN(age) || age < 8 || age > 120)
+      return res.status(500).json({ error: "Idade inválida. A idade deve ser um número entre 8 e 120 anos" })
+   
+    if (!username || typeof username !== 'string' || username.length < 6)
+      return res.status(500).json({ error: "Username inválido. Deve ser uma string com pelo menos 6 caracteres" })
+   
+    if (email && typeof email !== 'string')
+      return res.status(500).json({ error: "Email inválido" })
+   
+    if (country && typeof country !== 'string')
+      return res.status(500).json({ error: "País inválido" })
+
+  next()
+}
+
+const createUser = (req, res) => {
+  const { name, age, email, country, username } = req.body
+
+  const insertQuery = `
+    INSERT INTO users (name, email, username, age, country, registered_date)
+    VALUES (?, ?, ?, ?, ?, GETDATE())
+  `
+
+  const params = [name, email, username, age, country]
+
+  db.run(insertQuery, params, function (err) {
+    if (err)
+      return res.status(500).json({ error: "Erro ao criar o usuário" })
+
+    const maxIdQuery = `SELECT MAX(id) AS id_max FROM users`
+
+    db.get(maxIdQuery, (err, row) => {
+      if (err)
+        return res.status(500).json({ error: "ERRo" })
+
+      res.status(201).json({
+        message: 'Usuário criado com sucesso',
+        userId: row.id_max,
+      })
+    })
+  })
+}
+
 app.get('/', helloDev)
 app.get('/users', users)
 app.post('/users', validateUser, createUser)
 app.get('/users/search', checkingQuery, executeQuery)
-app.use(express.json())
 
 app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta: ${port}`))
