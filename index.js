d59b203089c71a62ba6d4423b3ee9e0773947383
Fabio <fabio12513@gmail.com>
2024-11-21 07:12:21 -0300

    2.2

--- index.js
+++ index.js
@@ -6,6 +6,58 @@ const port = 3005
 
 const db = new sqlite3.Database('./banco.db')
 
+const validateUser = (req, res, next) => {
+  const { name, email, age, registered_date, country, username } = req.body
+
+    if (!name || typeof name !== 'string' || !name.trim().includes(' '))
+      return res.status(500).json({ error: "Nome inválido. É necessário pelo menos dois nomes com um espaço entre eles" })
+    
+    if (!age || isNaN(age) || age < 8 || age > 120)
+      return res.status(500).json({ error: "Idade inválida. A idade deve ser um número entre 8 e 120 anos" })
+   
+    if (!registered_date || typeof registered_date !== 'string' || registered_date.length !== 10)
+      return res.status(500).json({ error: "Data inválida. A data deve ser uma string com 8 caracteres" })
+   
+    if (!username || typeof username !== 'string' || username.length < 6)
+      return res.status(500).json({ error: "Username inválido. Deve ser uma string com pelo menos 6 caracteres" })
+   
+    if (email && typeof email !== 'string')
+      return res.status(500).json({ error: "Email inválido" })
+   
+    if (country && typeof country !== 'string')
+      return res.status(500).json({ error: "País inválido" })
+
+  next()
+}
+
+const createUser = (req, res) => {
+  const { name, age, email, country, username, registered_date } = req.body
+
+  const insertQuery = `
+    INSERT INTO users (name, email, username, age, country, registered_date)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `
+
+  const params = [name, email, username, age, country, registered_date]
+
+  db.run(insertQuery, params, (err) => {
+    if (err)
+      return res.status(500).json({ error: "ERro ao criar o usuario" })
+
+    const countQuery = `SELECT COUNT(*) AS total FROM users`
+
+    db.get(countQuery, (err, row) => {
+      if (err)
+        return res.status(500).json({ error: "error" })
+
+      res.status(201).json({
+        message: 'Usuário criado com sucesso',
+        userId: row.total,
+      })
+    })
+  })
+}
+
 const helloDev = (req, res) => res.send('Hello Dev')
 
 const users = (req, res) => {
@@ -16,18 +68,15 @@ const users = (req, res) => {
 }
 
 const checkingQuery = (req, res, next) => {
-  const {
-    id, age, id_maiorq, id_menorq, age_maiorq, age_menorq
-  } = req.query
+  const { id, age, id_maiorq, id_menorq, age_maiorq, age_menorq } = req.query
 
-    if (id && id_maiorq) return res.status(501).send("Impossível buscar por: id e id_maiorq ao mesmo tempo!")
-    if (id && id_menorq) return res.status(501).send("Impossível buscar por: id e id_menorq ao mesmo tempo!")
+    if (id && id_maiorq) return res.status(400).send("Impossível buscar por: id e id_maiorq ao mesmo tempo!")
+    if (id && id_menorq) return res.status(400).send("Impossível buscar por: id e id_menorq ao mesmo tempo!")
 
-    if (age && age_maiorq) return res.status(501).send("Impossível buscar por: age e age_maiorq ao mesmo tempo!")
-    if (age && age_menorq) return res.status(501).send("Impossível buscar por: age e age_menorq ao mesmo tempo!")
+    if (age && age_maiorq) return res.status(400).send("Impossível buscar por: age e age_maiorq ao mesmo tempo!")
+    if (age && age_menorq) return res.status(400).send("Impossível buscar por: age e age_menorq ao mesmo tempo!")
 
   next()
-
 }
 
 const executeQuery = (req, res) => {
@@ -36,53 +85,37 @@ const executeQuery = (req, res) => {
     id_maiorq, id_menorq, age_maiorq, age_menorq, page = 1, limite = 5, sortBy = 'id', order = 'asc'
   } = req.query
 
-  let query = "SELECT * FROM users WHERE"
-  let primeiraCondição = true
+  let query = "SELECT * FROM users WHERE 1=1"
 
-  
-  const addCondition = (condition) => {
-    if (primeiraCondição) {
+    if (id) query += ` AND id = ${parseInt(id)}`
+    if (id_maiorq) query += ` AND id > ${parseInt(id_maiorq)}`
+    if (id_menorq) query += ` AND id < ${parseInt(id_menorq)}`
 
-      query += ` ${condition}`
-      primeiraCondição = false
+    if (name) query += ` AND name LIKE '%${name}%'`
+    if (email) query += ` AND email LIKE '%${email}%'`
+    if (username) query += ` AND username LIKE '%${username}%'`
+    if (country) query += ` AND country LIKE '%${country}%'`
 
-    } else {
+    if (age) query += ` AND age = ${parseInt(age)}`
+    if (age_maiorq) query += ` AND age > ${parseInt(age_maiorq)}`
+    if (age_menorq) query += ` AND age < ${parseInt(age_menorq)}`
 
-      query += ` AND ${condition}`
+  query += ` ORDER BY ${sortBy} ${order.toUpperCase()}`
 
-    }
-  }
+  const offset = (page - 1) * limite
+  query += ` LIMIT ${limite} OFFSET ${offset}`
 
-    if (id) addCondition(`id = ${id}`)
-    if (id_maiorq) addCondition(`id > ${id_maiorq}`)
-    if (id_menorq) addCondition(`id < ${id_menorq}`)
+  db.all(query, (err, rows) => {
 
-    if (name) addCondition(`name LIKE '%${name}%'`)
-    if (email) addCondition(`email LIKE '%${email}%'`)
-    if (username) addCondition(`username LIKE '%${username}%'`)
-    if (country) addCondition(`country LIKE '%${country}%'`)
-
-    if (age) addCondition(`age = ${parseInt(age)}`)
-    if (age_maiorq) addCondition(`age > ${parseInt(age_maiorq)}`)
-    if (age_menorq) addCondition(`age < ${parseInt(age_menorq)}`)
-
-    query += ` ORDER BY ${sortBy} ${order.toUpperCase()}`
-
-    const offset = (page - 1) * limite
-    
-    query += ` LIMIT ${limite} OFFSET ${offset}`
-
-    db.all(query, (err, rows) => {
-
-      if (err) return res.status(500).send("Erro ao executar a consulta!")
-      res.json(rows)
-
+    if (err) return res.status(500).send("Erro ao executar a consulta!")
+    res.json(rows)
   })
-  
 }
 
 app.get('/', helloDev)
 app.get('/users', users)
+app.post('/users', validateUser, createUser)
 app.get('/users/search', checkingQuery, executeQuery)
+app.use(express.json())
 
 app.listen(port, '0.0.0.0', () => console.log(`Servidor rodando na porta: ${port}`))
