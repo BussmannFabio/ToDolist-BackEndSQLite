cf3d30f236b94faec1543e174156e0e70caf7478
Fabio <fabio12513@gmail.com>
2024-11-22 10:19:43 -0300

    2.5

--- _tests/app.test.js
+++ _tests/app.test.js
@@ -1,10 +1,62 @@
 import request from 'supertest'
 import app from '../index.js'
 
-describe('Testando rotas do Express', () => {
+describe('Testando rota /', () => {
     it('Deve retornar "Hello Dev" na rota /', async () => {
         const res = await request(app).get('/')
         expect(res.statusCode).toEqual(200)
         expect(res.body.message).toEqual('Hello Dev')
     })
 })
+
+describe('Testando rota GET /users/search', () => {
+  
+    it('Deve retornar 200 e os resultados da busca', async () => {
+      const res = await request(app).get('/users/search')
+      
+      expect(res.statusCode).toEqual(200)
+      expect(Array.isArray(res.body)).toBe(true) 
+    })
+    
+    it('Deve retornar 400 se for especificado id e id_maiorq', async () => {
+
+      const res = await request(app)
+        .get('/users/search?id=1&id_maiorq=10')
+      
+      expect(res.statusCode).toEqual(400)
+      expect(res.text).toBe("Impossível buscar por: id e id_maiorq ao mesmo tempo!")
+
+    })
+
+    it('Deve retornar 400 se for especificado id e id_menorq', async () => {
+
+        const res = await request(app)
+          .get('/users/search?id=1&id_menorq=10')
+        
+        expect(res.statusCode).toEqual(400)
+        expect(res.text).toBe("Impossível buscar por: id e id_menorq ao mesmo tempo!")
+  
+      })
+
+
+    it('Deve retornar 400 se for especificado age e age_menorq', async () => {
+
+        const res = await request(app)
+          .get('/users/search?age=1&age_menorq=10')
+        
+        expect(res.statusCode).toEqual(400)
+        expect(res.text).toBe("Impossível buscar por: age e age_menorq ao mesmo tempo!")
+  
+      })
+
+
+    it('Deve retornar 400 se for especificado age e age_maiorq', async () => {
+
+        const res = await request(app)
+          .get('/users/search?age=1&age_maiorq=10')
+        
+        expect(res.statusCode).toEqual(400)
+        expect(res.text).toBe("Impossível buscar por: age e age_maiorq ao mesmo tempo!")
+  
+      })
+  })
