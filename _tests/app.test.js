944c500e1bc0c08c0a8bb48f66360e32c6aea745
Fabio <fabio12513@gmail.com>
2024-11-22 09:22:21 -0300

    2.5

--- _tests/app.test.js
+++ _tests/app.test.js
@@ -1,9 +1,9 @@
-import app from '../index.js';
+import app from '../index.js'
 
 describe('Testando rotas do Express', () => {
     it('Deve retornar "Hello Dev" na rota /', async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toHaveProperty('message', 'Hello Dev');
-    });
-});
+        const res = await request(app).get('/')
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toHaveProperty('message', 'Hello Dev')
+    })
+})
