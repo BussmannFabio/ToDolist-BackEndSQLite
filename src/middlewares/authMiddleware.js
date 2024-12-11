4b65ec0311d29a32ee9bf055e0c7a774eb95c07b
Fabio <fabio12513@gmail.com>
2024-12-11 09:01:28 -0300

    1.0(full)

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,15 +3,14 @@ import sqlite3 from 'sqlite3'
 const db = new sqlite3.Database('./banco.db')
 
 const authenticateUser = (req, res, next) => {
-  // Obter o token e o email dos cabeçalhos da requisição
-  const token = req.headers['authorization']?.split(' ')[1]  // Obtemos o token após 'Bearer'
+  // token e o email nos headers
+  const token = req.headers['authorization']?.split(' ')[1]  //token após 'Bearer'
   const email = req.headers['x-email']
 
   if (!token || !email) {
     return res.status(401).json({ error: 'Token ou email não fornecido' })
   }
 
-  // Verificar se o token é válido para o email fornecido
   const query = "SELECT * FROM tokens WHERE email = ? AND token = ?"
   
   db.get(query, [email, token], (err, row) => {
@@ -19,7 +18,6 @@ const authenticateUser = (req, res, next) => {
       return res.status(401).json({ error: 'Token inválido ou expirado' })
     }
 
-    // Se o token for válido, passamos o email do usuário para a requisição
     req.userEmail = row.email 
     next() 
   })
